a6641b3434db025734494974248069a4fc0ce5d9
Ross <rossgarfield@gmail.com>
2022-04-28 08:06:52 -0500

    commit tree db048445c4fe163a0696371961ce5b02de274d6d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+
+function findMatching(drivers, driverName){
+    return drivers.filter(function(name){
+     if(name.toLowerCase() === driverName.toLowerCase()){
+         return name
+     }
+    })
+}
